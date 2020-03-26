70d27b9cd3fab35b4de994eb4be90a573771d5cf
halilbahar <halil.s.bahar@gmail.com>
2020-03-26 15:32:14 +0100

    Init quarkus project

---
+++ src/test/java/at/htl/beeyond/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl.beeyond;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
