1a54fcd7bd13d4cde0324cdd507297816140965a
halilbahar <halil.s.bahar@gmail.com>
2020-03-30 00:14:29 +0200

    Deploy nginx with given replica count

--- src/main/java/at/htl/beeyond/service/DeploymentService.java
+++ src/main/java/at/htl/beeyond/service/DeploymentService.java
@@ -1,5 +1,8 @@
 package at.htl.beeyond.service;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.vertx.core.json.JsonObject;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
@@ -9,16 +12,20 @@ import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class DeploymentService {
 
     private Yaml yaml;
     private Jsonb jsonb;
+    private KubernetesClient client;
 
     public DeploymentService() {
         DumperOptions dumperOptions = new DumperOptions();
@@ -27,6 +34,21 @@ public class DeploymentService {
 
         this.yaml = new Yaml(dumperOptions);
         this.jsonb = JsonbBuilder.create();
+        this.client = new DefaultKubernetesClient();
+    }
+
+    public void deployNginx(int replicaCount) {
+        List<JsonObject> jsonObjects = readYaml("nginx-deployment.yml");
+
+        jsonObjects.get(1).getJsonObject("spec").put("replicas", replicaCount);
+
+        String yaml = jsonObjects.stream()
+                .map(JsonObject::getMap)
+                .map(this.yaml::dump)
+                .collect(Collectors.joining("---\n"));
+
+        InputStream stream = new ByteArrayInputStream((yaml.getBytes()));
+        List<HasMetadata> list = client.load(stream).inNamespace("default").createOrReplace();
     }
 
     private List<JsonObject> readYaml(String file) {
