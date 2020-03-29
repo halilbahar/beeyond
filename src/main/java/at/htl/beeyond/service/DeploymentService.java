6fa24df5efe21084e9a380475d173de657a9d1f8
halilbahar <halil.s.bahar@gmail.com>
2020-03-29 23:08:08 +0200

    Read and parse yaml files as JsonObjectBuilder

--- src/main/java/at/htl/beeyond/service/DeploymentService.java
+++ src/main/java/at/htl/beeyond/service/DeploymentService.java
@@ -4,8 +4,14 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class DeploymentService {
@@ -20,5 +26,20 @@ public class DeploymentService {
 
         this.yaml = new Yaml(dumperOptions);
         this.jsonb = JsonbBuilder.create();
+    }
+
+    private List<JsonObjectBuilder> readYaml(String file) {
+        List<JsonObjectBuilder> result = new LinkedList<>();
+
+        InputStream inputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("/templates/" + file);
+        Iterable<Object> objects = yaml.loadAll(inputStream);
+
+        for (Object object : objects) {
+            result.add(Json.createObjectBuilder((Map<String, Object>) object));
+        }
+
+        return result;
     }
 }
