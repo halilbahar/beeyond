5cb83f08a757f8652fe71770221e6f8e4b62dd2e
halilbahar <halil.s.bahar@gmail.com>
2020-03-30 00:20:38 +0200

    Remove unused imports

--- src/main/java/at/htl/beeyond/service/DeploymentService.java
+++ src/main/java/at/htl/beeyond/service/DeploymentService.java
@@ -8,16 +8,12 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.json.Json;
-import javax.json.JsonObjectBuilder;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
