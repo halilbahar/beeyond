03ba9ae87dff7b29d28b4f0402ab56bbd621f2c1
halilbahar <halil.s.bahar@gmail.com>
2020-03-29 22:30:07 +0200

    Declare yaml and jsonb in DeploymentService

--- src/main/java/at/htl/beeyond/service/DeploymentService.java
+++ src/main/java/at/htl/beeyond/service/DeploymentService.java
@@ -1,4 +1,24 @@
 package at.htl.beeyond.service;
 
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+@ApplicationScoped
 public class DeploymentService {
+
+    private Yaml yaml;
+    private Jsonb jsonb;
+
+    public DeploymentService() {
+        DumperOptions dumperOptions = new DumperOptions();
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        dumperOptions.setPrettyFlow(true);
+
+        this.yaml = new Yaml(dumperOptions);
+        this.jsonb = JsonbBuilder.create();
+    }
 }
