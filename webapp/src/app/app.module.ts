64c90b14289006e6968d43fed8719341e7f0bce9
halilbahar <halil.s.bahar@gmail.com>
2020-03-31 15:06:40 +0200

    Add angular materials

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { SideNavigationComponent } from './side-navigation/side-navigation.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
