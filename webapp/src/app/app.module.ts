ae74cae85b6a2fc51a1ed8aea8f326ca0e67b82f
halilbahar <halil.s.bahar@gmail.com>
2020-03-31 14:59:54 +0200

    Create side navigation component

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavigationComponent } from './side-navigation/side-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavigationComponent
   ],
   imports: [
     BrowserModule,
