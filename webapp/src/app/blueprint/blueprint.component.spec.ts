fa9190174de5cf6b29b1362bd228bb0787afd3d2
julinad <juli.naderer@gmail.com>
2020-04-01 18:37:44 +0200

    generate blueprint component

---
+++ webapp/src/app/blueprint/blueprint.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlueprintComponent } from './blueprint.component';
+
+describe('BlueprintComponent', () => {
+  let component: BlueprintComponent;
+  let fixture: ComponentFixture<BlueprintComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BlueprintComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BlueprintComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ webapp/src/app/blueprint/blueprint.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blueprint',
+  templateUrl: './blueprint.component.html',
+  styleUrls: ['./blueprint.component.css']
+})
+export class BlueprintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
