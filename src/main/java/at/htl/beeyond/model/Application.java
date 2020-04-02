091e81f0cfb9cc2502abacdf00c67e766aa1e67d
sonjacao <csonja1703@gmail.com>
2020-04-02 12:29:27 +0200

    Create class Application

---
+++ src/main/java/at/htl/beeyond/model/Application.java
@@ -0,0 +1,23 @@
+package at.htl.beeyond.model;
+
+public class Application {
+
+    private Long id;
+    private int replica;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getReplica() {
+        return replica;
+    }
+
+    public void setReplica(int replica) {
+        this.replica = replica;
+    }
+}
