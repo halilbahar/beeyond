bbf3aed7d77027ff4f38ced26b828ac14c81b799
sonjacao <csonja1703@gmail.com>
2020-04-02 13:06:46 +0200

    Add annotation for table 'Application'

--- src/main/java/at/htl/beeyond/model/Application.java
+++ src/main/java/at/htl/beeyond/model/Application.java
@@ -1,7 +1,15 @@
 package at.htl.beeyond.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Application {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int replica;
 
