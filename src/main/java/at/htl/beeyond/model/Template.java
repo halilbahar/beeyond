dad1e3a7bdffd62138dfc27d63ee81794afd482d
halilbahar <halil.s.bahar@gmail.com>
2020-04-27 13:02:53 +0200

    Create template entity

---
+++ src/main/java/at/htl/beeyond/model/Template.java
@@ -0,0 +1,58 @@
+package at.htl.beeyond.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Template {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private String name;
+    private String description;
+
+    public Template(String content, String name, String description) {
+        this.content = content;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Template() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
