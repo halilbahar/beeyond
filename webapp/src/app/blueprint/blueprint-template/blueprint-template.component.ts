9c866bc40943c9ce72e82d76bc65e18124a4cd5d
julinad <juli.naderer@gmail.com>
2020-04-30 12:13:44 +0200

    Generate blueprint-template

---
+++ webapp/src/app/blueprint/blueprint-template/blueprint-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blueprint-template',
+  templateUrl: './blueprint-template.component.html',
+  styleUrls: ['./blueprint-template.component.css']
+})
+export class BlueprintTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
