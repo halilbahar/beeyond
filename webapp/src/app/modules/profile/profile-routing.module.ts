8f4b1d80fdc16ee1b83604a731dfb9898aa6c87c
halilbahar <halil.s.bahar@gmail.com>
2020-05-23 23:40:29 +0200

    Move profile component to page folder

--- webapp/src/app/modules/profile/profile-routing.module.ts
+++ webapp/src/app/modules/profile/profile-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   {path: '', component: ProfileComponent},
--- webapp/src/app/modules/profile/profile.module.ts
+++ webapp/src/app/modules/profile/profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
-import { ProfileComponent } from './profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
