f6c548710cf63e520a5c6b9ac0d16b346e02381b
halilbahar <halil.s.bahar@gmail.com>
2020-05-23 23:39:45 +0200

    Move dashboard component to page folder

--- webapp/src/app/modules/dashboard/dashboard.module.ts
+++ webapp/src/app/modules/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
