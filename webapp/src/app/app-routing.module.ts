268a9f2384898fd135d3ce358d185f3582f91c49
halilbahar <halil.s.bahar@gmail.com>
2020-05-24 03:56:00 +0200

    Guard all routes expect login

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
-  {path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
-  {path: 'blueprint', loadChildren: () => import('./modules/blueprint/blueprint.module').then(m => m.BlueprintModule)},
-  {path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)},
-  {path: 'accounting', loadChildren: () => import('./modules/accounting/accounting.module').then(m => m.AccountingModule)},
-  {path: 'management', loadChildren: () => import('./modules/management/management.module').then(m => m.ManagementModule)}
+  {
+    path: '', canActivate: [AuthGuard], children: [
+      {path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
+      {path: 'blueprint', loadChildren: () => import('./modules/blueprint/blueprint.module').then(m => m.BlueprintModule)},
+      {path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)},
+      {path: 'accounting', loadChildren: () => import('./modules/accounting/accounting.module').then(m => m.AccountingModule)},
+      {path: 'management', loadChildren: () => import('./modules/management/management.module').then(m => m.ManagementModule)}
+    ]
+  },
+  {path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)}
 ];
 
 @NgModule({
