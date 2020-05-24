ac31a7e3a6080b6a2eb154e2bc9894075b3560dd
halilbahar <halil.s.bahar@gmail.com>
2020-05-24 03:39:30 +0200

    Create login page

--- webapp/src/app/modules/login/login.module.ts
+++ webapp/src/app/modules/login/login.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginRoutingModule } from './login-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
--- webapp/src/app/modules/login/pages/login/login.component.ts
+++ webapp/src/app/modules/login/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../../core/authentification/authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+  private returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private fb: FormBuilder
+  ) {
+    if (this.authenticationService.currentUserValue()) {
+      this.router.navigate(['/']).then(console.log);
+    }
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
+
+  submitUserLogin() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    const controls = this.loginForm.controls;
+    this.authenticationService.login(controls.username.value, controls.password.value)
+      .pipe(first())
+      .subscribe(data => this.router.navigate([this.returnUrl]));
+  }
 }
