49167843de94867e07a03b64a53bcba46497c5c5
halilbahar <halil.s.bahar@gmail.com>
2020-06-01 01:02:37 +0200

    Create CustomApplicationDto

---
+++ src/main/java/at/htl/beeyond/dto/CustomApplicationDto.java
@@ -0,0 +1,36 @@
+package at.htl.beeyond.dto;
+
+import at.htl.beeyond.entity.CustomApplication;
+import at.htl.beeyond.entity.User;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+public class CustomApplicationDto {
+
+    @Length(max = 255, message = "The note of the custom application cannot be longer than 255 characters")
+    private String note;
+
+    @NotBlank(message = "The content of the custom application cannot be empty")
+    private String content;
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public CustomApplication map(User owner) {
+        return new CustomApplication(note, owner, content);
+    }
+}
