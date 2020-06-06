db9beb1055e510be42eba958499ffa21171aa84a
halilbahar <halil.s.bahar@gmail.com>
2020-06-06 23:57:09 +0200

    Remove obsolete files

--- src/main/java/at/htl/beeyond/validation/checks/TemplateSequence.java
+++
@@ -1,8 +0,0 @@
-package at.htl.beeyond.validation.checks;
-
-import javax.validation.GroupSequence;
-import javax.validation.groups.Default;
-
-@GroupSequence({Default.class, TemplateContentCheck.class})
-public interface TemplateSequence {
-}
