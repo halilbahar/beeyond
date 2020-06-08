0e8774facefbdf39d0e6926b5a7416ff13190d12
sonjacao <csonja1703@gmail.com>
2020-06-08 11:05:58 +0200

    Add test method for creating a custom application

--- src/test/java/at/htl/beeyond/application/custom/CustomApplicationTest.java
+++ src/test/java/at/htl/beeyond/application/custom/CustomApplicationTest.java
@@ -5,42 +5,7 @@ import com.intuit.karate.junit5.Karate;
 public class CustomApplicationTest {
 
     @Karate.Test
-    Karate test001GetAll() {
-        return Karate.run("custom-application-get").relativeTo(getClass());
+    Karate testCreate() {
+        return Karate.run("custom-application-post").tags("validCustomApplication").relativeTo(getClass());
     }
-
-    @Karate.Test
-    Karate test002Create() {
-        return Karate.run("custom-application-post").tags("valid").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test003InvalidContent() { return Karate.run("custom-application-post").tags("invalidContent").relativeTo(getClass()); }
-
-    @Karate.Test
-    Karate test004InvalidNote() { return Karate.run("custom-application-post").tags("invalidNote").relativeTo(getClass()); }
-
-    @Karate.Test
-    Karate test005Approve() {
-        return Karate.run("custom-application-put").tags("validApprove").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test006InvalidApprove() { return Karate.run("custom-application-put").tags("invalidApprove").relativeTo(getClass()); }
-
-    @Karate.Test
-    Karate test007Deny() {
-        return Karate.run("custom-application-put").tags("validDeny").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test008InvalidDeny() { return Karate.run("custom-application-put").tags("invalidDeny").relativeTo(getClass()); }
-
-    @Karate.Test
-    Karate test009Delete() {
-        return Karate.run("custom-application-delete").tags("valid").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test010InvalidDelete() { return Karate.run("custom-application-delete").tags("invalid").relativeTo(getClass()); }
 }
