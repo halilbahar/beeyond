9f6b819e9e21ad1ed6a3a5d90fdb38b3270c43ea
sonjacao <csonja1703@gmail.com>
2020-06-08 09:38:55 +0200

    Define credentials of test users and base url in karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,1 +1,15 @@
-function fn(){ return {} }
+function fn(){
+    var env = karate.env;
+    karate.log('karate.env system property was:', env);
+    if (!env) {
+        env = 'dev';
+    }
+    var config = {
+        baseUrl: 'http://localhost:8080',
+        testTeacherCredentials: '{ username: "testteacher", password: "teacher"}',
+        testStudentCredentials: '{ username: "teststudent", password: "student"}'
+    };
+    karate.configure('connectTimeout', 5000);
+    karate.configure('readTimeout', 5000);
+    return config;
+}
