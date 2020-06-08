d078578a4a33e0dc3dd9163ae49e349a49508f36
sonjacao <csonja1703@gmail.com>
2020-06-08 11:35:01 +0200

    Add test method for creating a template application

--- src/test/java/at/htl/beeyond/application/template/TemplateApplicationTest.java
+++ src/test/java/at/htl/beeyond/application/template/TemplateApplicationTest.java
@@ -5,22 +5,7 @@ import com.intuit.karate.junit5.Karate;
 public class TemplateApplicationTest {
 
     @Karate.Test
-    Karate test001Upload() {
+    Karate testCreate() {
         return Karate.run("template-application-post").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test003GetAll() {
-        return Karate.run("template-application-get").tags("all").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test004GetById() {
-        return Karate.run("template-application-get").tags("validId").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test005GetByNotExistingId() {
-        return Karate.run("template-application-get").tags("invalidId").relativeTo(getClass());
     }
 }
