3c37c4aaf85c6167e539a97c257521341b226195
sonjacao <csonja1703@gmail.com>
2020-06-08 10:07:28 +0200

    Create test method for getting all templates

--- src/test/java/at/htl/beeyond/template/TemplateTest.java
+++ src/test/java/at/htl/beeyond/template/TemplateTest.java
@@ -5,22 +5,7 @@ import com.intuit.karate.junit5.Karate;
 public class TemplateTest {
 
     @Karate.Test
-    Karate test001GetAll() {
+    Karate testGetAll() {
         return Karate.run("template-get").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test002Create() {
-        return Karate.run("template-post").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test003Delete() {
-        return Karate.run("template-delete").tags("valid").relativeTo(getClass());
-    }
-
-    @Karate.Test
-    Karate test004InvalidDelete() {
-        return Karate.run("template-delete").tags("invalid").relativeTo(getClass());
     }
 }
