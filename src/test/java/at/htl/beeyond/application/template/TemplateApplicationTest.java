921173281841b5ae9ddbc45be420a03cea4e6ffc
sonjacao <csonja1703@gmail.com>
2020-06-10 10:23:27 +0200

    Add test method for creating a template application with a note length over 255

--- src/test/java/at/htl/beeyond/application/template/TemplateApplicationTest.java
+++ src/test/java/at/htl/beeyond/application/template/TemplateApplicationTest.java
@@ -6,6 +6,11 @@ public class TemplateApplicationTest {
 
     @Karate.Test
     Karate testCreate() {
-        return Karate.run("template-application-post").relativeTo(getClass());
+        return Karate.run("template-application-post").tags("create").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testNoteLength() {
+        return Karate.run("template-application-post").tags("noteLength").relativeTo(getClass());
     }
 }
