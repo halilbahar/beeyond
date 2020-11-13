1b3468b68027c4cec809c2df746bc4ab5d41bf10
MoritzEichhorn <moritz.eichhorn@outlook.com>
2020-11-13 16:02:36 +0100

    Add breadcrumbs to header
    Co-authored-by: mwllgr <25794895+mwllgr@users.noreply.github.com>

--- frontend-beeyond/src/app/core/header/header.component.ts
+++ frontend-beeyond/src/app/core/header/header.component.ts
@@ -9,14 +9,26 @@ import { NavigationEnd, Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   headerTitle = '';
+  breadcrumbs = [];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        const title = event.url.split('/')[1];
-        this.headerTitle = title.charAt(0).toUpperCase() + title.substr(1);
+        this.breadcrumbs = []
+        const links = event.url.split('/');
+        links.shift();
+
+        links.forEach(link => {
+          this.breadcrumbs.push({
+            link: link,
+            title: link.charAt(0).toUpperCase() + link.substr(1)
+          })
+        });
+
+        this.headerTitle = this.breadcrumbs[0].title;
+        console.log(this.breadcrumbs);
       }
     });
   }
