733c6d0d170138b74b0455445f7cc62614710f29
halilbahar <halil.s.bahar@gmail.com>
2020-11-24 20:48:11 +0100

    Disable oidc instead of starting a keycloak test container

--- backend-beeyond/src/test/java/at/htl/beeyond/intergration/application/custom/CustomApplicationTest.java
+++ backend-beeyond/src/test/java/at/htl/beeyond/intergration/application/custom/CustomApplicationTest.java
@@ -1,14 +1,12 @@
 package at.htl.beeyond.intergration.application.custom;
 
 import at.htl.beeyond.intergration.DatabaseResource;
-import at.htl.beeyond.intergration.IdentityProviderResource;
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 @QuarkusTestResource(DatabaseResource.class)
-@QuarkusTestResource(IdentityProviderResource.class)
 public class CustomApplicationTest {
 
     @Karate.Test
--- backend-beeyond/src/test/java/at/htl/beeyond/intergration/template/TemplateTest.java
+++ backend-beeyond/src/test/java/at/htl/beeyond/intergration/template/TemplateTest.java
@@ -1,7 +1,6 @@
 package at.htl.beeyond.intergration.template;
 
 import at.htl.beeyond.intergration.DatabaseResource;
-import at.htl.beeyond.intergration.IdentityProviderResource;
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,7 +8,6 @@ import io.quarkus.test.security.TestSecurity;
 
 @QuarkusTest
 @QuarkusTestResource(DatabaseResource.class)
-@QuarkusTestResource(IdentityProviderResource.class)
 public class TemplateTest {
 
     @Karate.Test
