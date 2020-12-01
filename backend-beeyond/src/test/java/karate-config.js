8e5349201a5237f1ce289562fa21cb183f385306
MoritzEichhorn <moritz.eichhorn@outlook.com>
2020-12-01 16:31:24 +0100

    Configure static users for backup authentication

--- backend-beeyond/src/test/java/karate-config.js
+++ backend-beeyond/src/test/java/karate-config.js
@@ -5,7 +5,19 @@ function fn(){
         env = 'dev';
     }
     var config = {
-        baseUrl: 'http://localhost:8081'
+        baseUrl: 'http://localhost:8081',
+        teacherAuth: function() {
+            var temp = 'stuetz:password';
+            var Base64 = Java.type('java.util.Base64');
+            var encoded = Base64.getEncoder().encodeToString(temp.bytes);
+            return 'Basic ' + encoded;
+        },
+        studentAuth: function() {
+            var temp = 'moritz:password';
+            var Base64 = Java.type('java.util.Base64');
+            var encoded = Base64.getEncoder().encodeToString(temp.bytes);
+            return 'Basic ' + encoded;
+        }
     };
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
