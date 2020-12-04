99e308e578c8b408779a524a114eab981660b947
MoritzEichhorn <moritz.eichhorn@outlook.com>
2020-12-04 17:44:31 +0100

    Add get application tests and create 2 example applications before tests

--- backend-beeyond/src/test/java/at/htl/beeyond/intergration/application/ApplicationTest.java
+++ backend-beeyond/src/test/java/at/htl/beeyond/intergration/application/ApplicationTest.java
@@ -4,17 +4,31 @@ import at.htl.beeyond.intergration.DatabaseResource;
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
 
 @QuarkusTest
 @QuarkusTestResource(DatabaseResource.class)
 public class ApplicationTest {
+    @Karate.Test
+    @TestSecurity(user = "sonja-teacher", roles = "teacher")
+    Karate createTeacherApplication() {
+        return Karate.run("create-applications").tags("teacher").relativeTo(getClass());
+    }
 
     @Karate.Test
+    @TestSecurity(user = "moritz", roles = "student")
+    Karate createStudentApplication() {
+        return Karate.run("create-applications").tags("student").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    @TestSecurity(user = "sonja-teacher", roles = "teacher")
     Karate testGetAllAsTeacher() {
         return Karate.run("application-get").tags("teacher").relativeTo(getClass());
     }
 
     @Karate.Test
+    @TestSecurity(user = "moritz", roles = "student")
     Karate testGetAllAsStudent() {
         return Karate.run("application-get").tags("student").relativeTo(getClass());
     }
