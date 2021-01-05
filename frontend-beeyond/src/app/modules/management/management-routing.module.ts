056fa9b8b5f88ab1b987d3aafec6f740a9e43ab7
halilbahar <halil.s.bahar@gmail.com>
2021-01-05 20:52:54 +0100

    Move Resolver variable to its module

--- frontend-beeyond/src/app/modules/management/management-routing.module.ts
+++ frontend-beeyond/src/app/modules/management/management-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ApplicationComponent } from './pages/application/application.component';
 import { ApplicationReviewComponent } from './pages/application-review/application-review.component';
 import { ApplicationReviewResolver } from 'src/app/core/resolver/application-review.resolver';
+import { ApplicationResolver } from 'src/app/core/resolver/application.resolver';
 
 const routes: Routes = [
-  { path: '', component: ApplicationComponent },
+  { path: '', component: ApplicationComponent, resolve: { applications: ApplicationResolver } },
   {
     path: 'review/:id',
     component: ApplicationReviewComponent,
