cf3a25e104d2cd283f327b68e3da985558a769c7
mwllgr <25794895+mwllgr@users.noreply.github.com>
2021-01-26 11:38:21 +0100

    Add shell scripts for easier starting

---
+++ sh/frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+##################################
+#  This script requires screen!  #
+#     sudo apt install screen    #
+##################################
+
+# Press Ctrl + A, then D to exit
+
+screen -r frontend
