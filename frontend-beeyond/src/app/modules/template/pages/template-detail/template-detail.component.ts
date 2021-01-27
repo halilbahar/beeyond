c3ab89019845226bd4ad670ffae37faa1f9937b4
sonjacao <csonja1703@gmail.com>
2021-01-27 08:38:24 +0100

    Create resolver for getting a template

--- frontend-beeyond/src/app/modules/template/pages/template-detail/template-detail.component.ts
+++ frontend-beeyond/src/app/modules/template/pages/template-detail/template-detail.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Template } from '../../../../shared/models/template.model';
-import { ApiService } from '../../../../core/services/api.service';
 
 @Component({
   selector: 'app-template-detail',
@@ -9,22 +8,13 @@ import { ApiService } from '../../../../core/services/api.service';
   styleUrls: ['./template-detail.component.scss']
 })
 export class TemplateDetailComponent implements OnInit {
-  @Input() routePath = '';
+  template: Template | any;
 
-  template: Template;
-  id: number;
   monacoEditorOptions = { language: 'yaml', scrollBeyondLastLine: false, readOnly: true };
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private apiService: ApiService
-  ) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.id = Number(this.route.snapshot.paramMap.get('id'));
-    this.apiService.getTemplateById(this.id).subscribe(template => {
-      this.template = template;
-    });
+    this.template = this.route.snapshot.data.template;
   }
 }
--- frontend-beeyond/src/app/modules/template/template-routing.module.ts
+++ frontend-beeyond/src/app/modules/template/template-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TemplateComponent } from './pages/template/template.component';
 import { TemplateCreateComponent } from './pages/template-create/template-create.component';
 import { TemplateDetailComponent } from './pages/template-detail/template-detail.component';
+import { TemplateResolver } from '../../core/resolver/template-resolver';
 
 const routes: Routes = [
   { path: '', component: TemplateComponent },
   { path: 'create', component: TemplateCreateComponent },
-  { path: 'detail/:id', component: TemplateDetailComponent }
+  {
+    path: 'detail/:id',
+    component: TemplateDetailComponent,
+    resolve: { template: TemplateResolver }
+  }
 ];
 
 @NgModule({
