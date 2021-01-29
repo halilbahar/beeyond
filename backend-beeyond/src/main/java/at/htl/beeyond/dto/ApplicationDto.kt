dbd85f19009c74c58afb1354b33ccaaa6a780d14
halilbahar <halil.s.bahar@gmail.com>
2021-01-29 02:13:00 +0100

    Simplify ApplicationDto.kt

--- backend-beeyond/src/main/java/at/htl/beeyond/dto/ApplicationDto.kt
+++ backend-beeyond/src/main/java/at/htl/beeyond/dto/ApplicationDto.kt
@@ -5,29 +5,10 @@ import java.time.LocalDateTime
 import javax.json.bind.annotation.JsonbTransient
 import javax.validation.constraints.Size
 
-abstract class ApplicationDto {
-    open var id: Long? = null
-        protected set
-
-    @field:Size(max = 255)
-    var note: String? = null
-
-    @set:JsonbTransient
-    var status: ApplicationStatus? = null
-
-    @set:JsonbTransient
-    var owner: UserDto? = null
-
-    @set:JsonbTransient
-    var createdAt: LocalDateTime? = null
-
-    constructor(id: Long?, note: String?, status: ApplicationStatus?, owner: UserDto?, createdAt: LocalDateTime?) {
-        this.id = id
-        this.note = note
-        this.status = status
-        this.owner = owner
-        this.createdAt = createdAt
-    }
-
-    constructor() {}
-}
+abstract class ApplicationDto(
+        var id: Long? = null,
+        @field:Size(max = 255) var note: String? = null,
+        @set:JsonbTransient var status: ApplicationStatus? = null,
+        @set:JsonbTransient var owner: UserDto? = null,
+        @set:JsonbTransient var createdAt: LocalDateTime? = null
+)
