5322719061cb0d6023fe06ccd5a23c672afb0032
halilbahar <halil.s.bahar@gmail.com>
2021-01-29 01:20:08 +0100

    Use enum instead of string for consumes / produces

--- backend-beeyond/src/main/java/at/htl/beeyond/resource/CustomApplicationResource.kt
+++ backend-beeyond/src/main/java/at/htl/beeyond/resource/CustomApplicationResource.kt
@@ -11,12 +11,13 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.SecurityContext
 
 @Path("/application/custom")
-@Consumes("application/json")
-@Produces("application/json")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 class CustomApplicationResource {
 
     @POST
