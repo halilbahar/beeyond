4efda7476c9b3809d227708987b675cedfe7e2d9
halilbahar <halil.s.bahar@gmail.com>
2021-01-31 18:06:15 +0100

    Generate constraint module

---
+++ frontend-beeyond/src/app/modules/constraints/constraint.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConstraintRoutingModule } from './constraint-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ConstraintRoutingModule]
+})
+export class ConstraintModule {}
