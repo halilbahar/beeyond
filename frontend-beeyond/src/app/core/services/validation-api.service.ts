9254a4d63f5a7ce4a68dd7c1aa024e484af49275
halilbahar <halil.s.bahar@gmail.com>
2021-02-01 00:23:59 +0100

    Get the root constraints

--- frontend-beeyond/src/app/core/services/validation-api.service.ts
+++ frontend-beeyond/src/app/core/services/validation-api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Schema } from 'src/app/shared/models/schema.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getRootConstraints(): Observable<Schema[]> {
+    return this.http.get<Schema[]>(`${environment.validationApiUrl}/constraints`);
+  }
 }
