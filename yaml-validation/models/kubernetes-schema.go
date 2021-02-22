a3de03439c2572f581dbf55dba418fb128c22dc7
halilbahar <halil.s.bahar@gmail.com>
2021-02-22 01:30:12 +0100

    Simplify yaml validation with creating common functions and structure methods

--- yaml-validation/models/kubernetes-schema.go
+++ yaml-validation/models/kubernetes-schema.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 	"yaml-validation/pkg/setting"
 )
 
@@ -40,6 +41,21 @@ type GroupKindVersion struct {
 	Version string `json:"version"`
 }
 
+type PathNotFoundError struct{}
+
+func (p PathNotFoundError) Error() string {
+	return "Path not found"
+}
+
+func (groupKindVersion GroupKindVersion) ToLower() GroupKindVersion {
+	var groupKindVersionLower GroupKindVersion
+	groupKindVersionLower.Group = strings.ToLower(groupKindVersion.Group)
+	groupKindVersionLower.Kind = strings.ToLower(groupKindVersion.Kind)
+	groupKindVersionLower.Version = strings.ToLower(groupKindVersion.Version)
+
+	return groupKindVersionLower
+}
+
 func GetSchemaCollection() (*SchemaCollection, error) {
 	baseUrl := setting.KubernetesJsonschemaSetting.Url
 	kubernetesVersion := setting.KubernetesJsonschemaSetting.KubernetesVersion
@@ -55,4 +71,92 @@ func GetSchemaCollection() (*SchemaCollection, error) {
 	}
 
 	return collection, nil
+}
+
+func GetSchemaBySegments(segments []string) (*Schema, error) {
+	collection, err := GetSchemaCollection()
+	if err != nil {
+		return nil, err
+	}
+
+	var currentSchema *Schema
+	for i, segment := range segments {
+		// On the first element search for the GroupKindVersion
+		if i == 0 {
+		schemaLoop:
+			for _, schema := range collection.Schemas {
+				for _, groupKindVersion := range schema.GroupKindVersion {
+					var group string
+					if groupKindVersion.Group == "" {
+						group = ""
+					} else {
+						group = "-" + groupKindVersion.Group
+					}
+
+					groupKindVersionString := groupKindVersion.Kind + group + "-" + groupKindVersion.Version
+					if strings.ToLower(segment) == strings.ToLower(groupKindVersionString) {
+						currentSchema = schema
+						break schemaLoop
+					}
+				}
+			}
+
+			if currentSchema == nil {
+				return nil, PathNotFoundError{}
+			}
+		} else {
+			property := currentSchema.Properties[segment]
+
+			if property == nil {
+				return nil, PathNotFoundError{}
+			}
+
+			var referencePath string
+			if property.Reference != "" {
+				referencePath = property.Reference
+			} else if property.Items != nil {
+				referencePath = property.Items.Reference
+			}
+
+			// If the specified path of the user does not exist, return
+			// This means the user requested something other than object (string, int, ...)
+			if referencePath == "" {
+				return nil, PathNotFoundError{}
+			}
+
+			// We want the last part of the reference
+			// Example: #/definitions/io.k8s.api.apps.v1.DeploymentSpec
+			split := strings.Split(referencePath, "/")
+			definitionName := split[len(split)-1]
+
+			currentSchema = collection.Schemas[definitionName]
+		}
+	}
+
+	groupKindVersion, constraintPath := GetGroupKindVersionAndPathFromSegments(segments)
+
+	for propertyName, property := range currentSchema.Properties {
+		if constraintPath == "" {
+			property.Constraint = GetConstraint(strings.ToLower(propertyName), &groupKindVersion)
+		} else {
+			property.Constraint = GetConstraint(constraintPath+"."+strings.ToLower(propertyName), &groupKindVersion)
+		}
+	}
+
+	return currentSchema, nil
+}
+
+func GetGroupKindVersionAndPathFromSegments(segments []string) (GroupKindVersion, string) {
+	var groupKindVersion GroupKindVersion
+	parts := strings.Split(segments[0], "-")
+	groupKindVersion.Kind = parts[0]
+	if len(parts) == 3 {
+		groupKindVersion.Group = parts[1]
+		groupKindVersion.Version = parts[2]
+	} else {
+		groupKindVersion.Version = parts[1]
+	}
+
+	constraintPath := strings.Join(segments[1:], ".")
+	return groupKindVersion, constraintPath
 }
