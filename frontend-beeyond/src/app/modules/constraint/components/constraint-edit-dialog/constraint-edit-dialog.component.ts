7af742ee6298f15c4831fc107a02ce7f617dbec9
halilbahar <halil.s.bahar@gmail.com>
2021-03-01 23:20:04 +0100

    Load intial values for form

--- frontend-beeyond/src/app/modules/constraint/components/constraint-edit-dialog/constraint-edit-dialog.component.ts
+++ frontend-beeyond/src/app/modules/constraint/components/constraint-edit-dialog/constraint-edit-dialog.component.ts
@@ -33,43 +33,27 @@ export class ConstraintEditDialogComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    const { enum: enumArray, min, max, regex } = this.data.constraint || {};
     const group: { [key: string]: any } = {};
     if (this.isRegexAllowed()) {
-      group.regex = [''];
+      group.regex = [regex || null];
     }
     if (this.isEnumAllowed()) {
-      group.enum = [[]];
+      group.enum = [enumArray || []];
     }
     if (this.isMinMaxAllowed()) {
-      group.min = [];
-      group.max = [];
+      group.min = [min || null];
+      group.max = [max || null];
     }
 
     this.form = this.fb.group(group);
     this.form.setValidators(this.validConstraint());
+    // If the controls have inti
+    const valueChange = this.onValueChange();
+    valueChange();
 
     // Disable other controls when 1 control has a value
-    this.form.valueChanges.subscribe(value => {
-      if (!this.disablingForm) {
-        this.disablingForm = true;
-
-        if ((this.form.controls.regex?.value || '') !== '') {
-          this.form.controls.enum?.disable();
-        } else if (this.form.controls.enum?.value.length > 0) {
-          this.form.controls.regex?.disable();
-          this.form.controls.min?.disable();
-          this.form.controls.max?.disable();
-        } else if (
-          (this.form.controls.min?.value || '') !== '' ||
-          (this.form.controls.max?.value || '') !== ''
-        ) {
-          this.form.controls.enum?.disable();
-        } else {
-          this.form.enable();
-        }
-        this.disablingForm = false;
-      }
-    });
+    this.form.valueChanges.subscribe(valueChange);
   }
 
   get enumControl(): FormControl {
@@ -125,6 +109,30 @@ export class ConstraintEditDialogComponent implements OnInit {
 
   private isMinMaxAllowed(): boolean {
     return this.data.type === 'integer';
+  }
+
+  private onValueChange(): () => void {
+    return () => {
+      if (!this.disablingForm) {
+        this.disablingForm = true;
+
+        if ((this.form.controls.regex?.value || '') !== '') {
+          this.form.controls.enum?.disable();
+        } else if (this.form.controls.enum?.value.length > 0) {
+          this.form.controls.regex?.disable();
+          this.form.controls.min?.disable();
+          this.form.controls.max?.disable();
+        } else if (
+          (this.form.controls.min?.value || '') !== '' ||
+          (this.form.controls.max?.value || '') !== ''
+        ) {
+          this.form.controls.enum?.disable();
+        } else {
+          this.form.enable();
+        }
+        this.disablingForm = false;
+      }
+    };
   }
 
   private validConstraint(): ValidatorFn {
