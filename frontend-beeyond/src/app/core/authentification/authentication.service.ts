22303ac9cc6bc65c1f2e9d0e6ae5c11467d6118d
emisljivic@hotmail.com <emisljivic@hotmail.com>
2021-03-01 03:35:04 +0100

    fix format problem

--- frontend-beeyond/src/app/core/authentification/authentication.service.ts
+++ frontend-beeyond/src/app/core/authentification/authentication.service.ts
@@ -39,9 +39,7 @@ export class AuthenticationService {
     const jsonPayload = decodeURIComponent(
       atob(base64)
         .split('')
-        .map(c => {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
+        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
         .join('')
     );
 
