e98bba67c69ba0ff14aefa3a7f10629b32abe0d4
halilbahar <halil.s.bahar@gmail.com>
2021-04-14 01:54:04 +0200

    Create tabs for blueprint and namespace

--- frontend-beeyond/src/app/modules/profile/profile.module.ts
+++ frontend-beeyond/src/app/modules/profile/profile.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule]
+  imports: [CommonModule, ProfileRoutingModule, MatCardModule, MatTabsModule]
 })
-export class ProfileModule {}
+export class ProfileModule { }
