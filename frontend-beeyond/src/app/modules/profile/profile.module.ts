5bbe7e05012b3d3dab1f40055f99ae8831fe24cb
halilbahar <halil.s.bahar@gmail.com>
2021-04-14 02:42:47 +0200

    List all namespaces for the user

--- frontend-beeyond/src/app/modules/profile/profile.module.ts
+++ frontend-beeyond/src/app/modules/profile/profile.module.ts
@@ -4,9 +4,23 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { NamespaceListComponent } from './components/namespace-list/namespace-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, MatCardModule, MatTabsModule]
+  declarations: [ProfileComponent, NamespaceListComponent],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    MatCardModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
+  ]
 })
 export class ProfileModule { }
---
+++ frontend-beeyond/src/app/shared/models/namespace.model.ts
@@ -0,0 +1,7 @@
+import { User } from './user.model';
+
+export interface Namespace {
+    id: number;
+    namespace: string;
+    users: User[];
+}
