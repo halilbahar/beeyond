a36612c2cf27e349f3e51d90350470620534a1e0
halilbahar <halil.s.bahar@gmail.com>
2021-04-14 17:38:59 +0200

    Mock the validation api instead of starting it

--- backend-beeyond/src/test/java/at/htl/beeyond/integration/application/template/TemplateApplicationTest.java
+++ backend-beeyond/src/test/java/at/htl/beeyond/integration/application/template/TemplateApplicationTest.java
@@ -1,14 +1,14 @@
 package at.htl.beeyond.integration.application.template;
 
 import at.htl.beeyond.integration.util.DatabaseResource;
-import at.htl.beeyond.integration.util.ValidationResource;
+import at.htl.beeyond.integration.util.WiremockValidation;
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 @QuarkusTestResource(DatabaseResource.class)
-@QuarkusTestResource(ValidationResource.class)
+@QuarkusTestResource(WiremockValidation.class)
 public class TemplateApplicationTest {
 
     @Karate.Test
---
+++ backend-beeyond/src/test/java/at/htl/beeyond/integration/util/WiremockValidation.java
@@ -0,0 +1,31 @@
+package at.htl.beeyond.integration.util;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class WiremockValidation implements QuarkusTestResourceLifecycleManager {
+
+    private WireMockServer wireMockServer;
+
+    @Override
+    public Map<String, String> start() {
+        this.wireMockServer = new WireMockServer();
+        this.wireMockServer.start();
+
+        stubFor(post(urlEqualTo("/api/validate")).willReturn(aResponse().withStatus(200)));
+
+        return Collections.singletonMap("at.htl.beeyond.service.ValidationRestClient/mp-rest/url", wireMockServer.baseUrl());
+    }
+
+    @Override
+    public void stop() {
+        if (null != wireMockServer) {
+            wireMockServer.stop();
+        }
+    }
+}
