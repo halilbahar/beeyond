f0f5924c7bfea9224c9a3f35c7ca474bf194ffc9
halilbahar <halil.s.bahar@gmail.com>
2021-04-21 08:24:48 +0200

    List all namespaces

--- frontend-beeyond/src/app/modules/namespace/pages/namespace-list/namespace-list.component.ts
+++ frontend-beeyond/src/app/modules/namespace/pages/namespace-list/namespace-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BackendApiService } from 'src/app/core/services/backend-api.service';
+import { Namespace } from 'src/app/shared/models/namespace.model';
+import { NamespaceDialogComponent } from '../../components/namespace-dialog/namespace-dialog.component';
 
 @Component({
   selector: 'app-namespace-list',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./namespace-list.component.scss']
 })
 export class NamespaceListComponent implements OnInit {
+  namespaces: Namespace[];
 
-  constructor() { }
+  constructor(private backendApiService: BackendApiService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.refreshNamespaces();
   }
 
+  createNamespaceDialog(): void {
+    const dialogRef = this.dialog.open(NamespaceDialogComponent, { width: '50vw', autoFocus: false });
+    
+    dialogRef.afterClosed().subscribe(saved => {
+      if (saved) {
+        this.refreshNamespaces();
+      }
+    });
+  }
+
+  editNamespaceDialog(namespace: Namespace): void {
+    this.dialog.open(NamespaceDialogComponent, {
+      width: '50vw',
+      autoFocus: false,
+      data: namespace
+    });
+  }
+
+  private refreshNamespaces(): void {
+    this.backendApiService.getNamespaces().subscribe(namespaces => {
+      this.namespaces = namespaces;
+    });
+  }
 }
