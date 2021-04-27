c4a5f468295255f9a79cd47f71ff83d1acabc839
emisljivic@hotmail.com <emisljivic@hotmail.com>
2021-04-27 14:00:55 +0200

    #bee-135: Format

--- frontend-beeyond/src/app/core/guards/auth.guard.ts
+++ frontend-beeyond/src/app/core/guards/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(this.authenticationService.roles.getValue().includes(next.data.requiredRole)){
+    if (this.authenticationService.roles.getValue().includes(next.data.requiredRole)) {
       return true;
     }
 
