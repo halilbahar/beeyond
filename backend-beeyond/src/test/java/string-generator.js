75a3539008b1eec5e5dd1585150af7409a36f8c9
emisljivic@hotmail.com <emisljivic@hotmail.com>
2021-11-19 13:25:11 +0100

    bee-171 Modify karate tests

--- backend-beeyond/src/test/java/string-generator.js
+++ backend-beeyond/src/test/java/string-generator.js
@@ -1,6 +1,6 @@
 function fn() {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < 300; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
