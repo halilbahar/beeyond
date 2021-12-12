9c90895542db21af08dac624d344093f29f60bee
eminasljivic <emisljivic@hotmail.com>
2021-12-12 02:08:10 +0100

    bee-188 Bump quarkus

--- backend-beeyond/src/main/java/at/htl/beeyond/service/ValidationRestClient.java
+++ backend-beeyond/src/main/java/at/htl/beeyond/service/ValidationRestClient.java
@@ -3,7 +3,7 @@ package at.htl.beeyond.service;
 import at.htl.beeyond.dto.FailedFieldDto;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.ws.rs.Consumes;
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.Set;
 
 @RegisterRestClient
+@ApplicationScoped
 public interface ValidationRestClient {
 
     @POST
