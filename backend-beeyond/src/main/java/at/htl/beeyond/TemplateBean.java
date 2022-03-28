b384bf3ebc5771caa1950771f65dca8b291b17d5
MoritzEichhorn <moritz.eichhorn@outlook.com>
2022-03-28 20:58:41 +0200

    bee-226 Fix TemplateBean error when started in docker

--- backend-beeyond/src/main/java/at/htl/beeyond/TemplateBean.java
+++ backend-beeyond/src/main/java/at/htl/beeyond/TemplateBean.java
@@ -3,36 +3,48 @@ package at.htl.beeyond;
 import at.htl.beeyond.entity.Template;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.json.JsonReader;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 class TemplateBean {
+    private static final String[] FILENAMES = { "quarkus" };
     @Transactional
-    void init(@Observes StartupEvent event) throws URISyntaxException, IOException {
-        var uri = getClass().getResource("/templates/json").toURI();
-        var dirPath = Paths.get(uri);
-        Files.list(dirPath).forEach(fileName -> {
-            try {
-                var jsonString = Files.readString(Paths.get(fileName.toString()));
+    void init(@Observes StartupEvent event) throws IOException {
+        for(String filename : FILENAMES) {
+            try (InputStream in = getClass()
+                    .getResourceAsStream("/templates/json/" + filename + ".json")){
+                assert in != null;
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                var jsonString = reader.lines().collect(Collectors.joining("\n"));
                 var newTemplate = new ObjectMapper().readValue(jsonString, Template.class);
+
+                InputStream contentIn = getClass()
+                        .getResourceAsStream("/templates/yml/" + newTemplate.getContent());
+                assert contentIn != null;
+                BufferedReader contentReader = new BufferedReader(new InputStreamReader(contentIn));
                 newTemplate.setContent(
-                        Files.readString(Paths.get(getClass().getResource("/templates/yml/"+newTemplate.getContent()).toURI()))
+                        contentReader.lines().collect(Collectors.joining("\n"))
                 );
+                contentIn.close();
                 newTemplate.getFields().forEach(fieldName ->{
                     fieldName.setTemplate(newTemplate);
                 });
                 newTemplate.persist();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        });
+        }
     }
 }
