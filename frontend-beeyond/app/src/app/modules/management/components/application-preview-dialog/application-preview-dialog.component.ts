b02242ebf97cc5f19567ab0fea198bf53ae35e23
eminasljivic <emisljivic@hotmail.com>
2022-04-07 17:22:36 +0200

    bee-212 Add height

--- frontend-beeyond/app/src/app/modules/management/components/application-preview-dialog/application-preview-dialog.component.ts
+++ frontend-beeyond/app/src/app/modules/management/components/application-preview-dialog/application-preview-dialog.component.ts
@@ -1,6 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ApplicationRange } from 'src/app/shared/models/application-range.model';
+import { ThemeService } from '../../../../core/services/theme.service';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { BaseComponent } from '../../../../core/services/base.component';
 
 declare let monaco: any;
 
@@ -10,15 +13,28 @@ declare let monaco: any;
   styleUrls: ['./application-preview-dialog.component.scss']
 })
 export class ApplicationPreviewDialogComponent {
-  monacoEditorOptions = { language: 'yaml', scrollBeyondLastLine: false, readOnly: true };
+  monacoEditorOptions = {
+    language: 'yaml',
+    scrollBeyondLastLine: false,
+    readOnly: true,
+    theme: this.themeService.isDarkTheme.value ? 'vs-dark' : 'vs-light'
+  };
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
       content: string;
       ranges: ApplicationRange[];
-    }
-  ) {}
+    },
+    private themeService: ThemeService
+  ) {
+    this.themeService.isDarkTheme.subscribe(value => {
+      this.monacoEditorOptions = {
+        ...this.monacoEditorOptions,
+        theme: value ? 'vs-dark' : 'vs-light'
+      };
+    });
+  }
 
   onEditorInit(editor: any) {
     const decorations = this.data.ranges.map(applicationRange => ({
