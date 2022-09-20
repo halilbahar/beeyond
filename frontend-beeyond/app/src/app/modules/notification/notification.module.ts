26819a43ece190bf7537b9338151827d5175b8d0
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-09-20 15:22:07 +0200

    bee-218 Update delete button design

--- frontend-beeyond/app/src/app/modules/notification/notification.module.ts
+++ frontend-beeyond/app/src/app/modules/notification/notification.module.ts
@@ -5,9 +5,10 @@ import { NotificationRoutingModule } from './notification-routing.module';
 import { NotificationComponent } from './pages/notification/notification.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NotificationComponent],
-  imports: [CommonModule, NotificationRoutingModule, MatIconModule, MatCardModule]
+  imports: [CommonModule, NotificationRoutingModule, MatIconModule, MatCardModule, MatButtonModule]
 })
 export class NotificationModule {}
